e16808900fbc39e310ef0e4913d313ef715841da
William Silva <williamsilva2@outlook.com>
2021-04-27 17:49:00 -0300

    Até aula 3.11 - Adicionando comportamento ao componente para manipular o estado.

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    nome: 'Will Silva'
+  }
+
+  modificarNome = (event) => {    
+    this.setState({
+      nome: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h1>Hello {this.state.nome}, isso é um título.</h1>
+        <h2>um subtítulo</h2>      
+      </>
+    )
+  }
 }
 
 export default App;
