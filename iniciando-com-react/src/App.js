91ec68b80192a1c020bae7031fa17627b952d902
William Silva <williamsilva2@outlook.com>
2021-04-27 18:30:21 -0300

    3.13 - Props.

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 class App extends React.Component {
 
   state = {
-    nome: 'Will Silva'
+    nome: ''
   }
 
   modificarNome = (event) => {    
@@ -29,7 +29,7 @@ class App extends React.Component {
     return (
       <>
         <input type="text" value={this.state.nome} onChange={this.modificarNome} />
-        <h1>Hello {this.state.nome}, isso é um título.</h1>
+        <h1>Hello {this.props.nome} sua idade é {this.props.idade} isso é um título.</h1>
         <h2>um subtítulo</h2>      
         <MeuComboBox/>
       </>
