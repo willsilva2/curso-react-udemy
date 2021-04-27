5bd1da899928e6acce829da4e491e28f61c8c2a0
William Silva <williamsilva2@outlook.com>
2021-04-27 18:45:27 -0300

    3.15 - Mais detalhes sobre componentes funcionais

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -1,18 +1,12 @@
 import React from 'react'
 
-class App extends React.Component {
+function App (props) {
 
-  state = {
-    nome: ''
+  const modificarNome = event => {
+    console.log(event.target.value);
   }
 
-  modificarNome = (event) => {    
-    this.setState({
-      nome: event.target.value
-    })
-  }
-
-  criarComboBox = () => {
+  const criarComboBox = () => {
     const opcoes = ['SP','RJ']
     const comboBoxOpcoes = opcoes.map( opcao => <option>{opcao}</option> )
 
@@ -22,24 +16,18 @@ class App extends React.Component {
       </select>
     )
   }
-
-  componentDidMount() {
-    console.log('Executou DidMount');
-  }
-
-  render() {
-    console.log('Executou render');
-    const MeuComboBox = () => this.criarComboBox()
-    
-    return (
-      <>
-        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
-        <h1>Hello {this.props.nome} sua idade é {this.props.idade} isso é um título.</h1>
-        <h2>um subtítulo</h2>      
-        <MeuComboBox/>
-      </>
-    )
-  }
+  
+  const MeuComboBox = () => criarComboBox()
+  
+  return (
+    <>
+      <input type="text" value={props.nome} onChange={modificarNome} />
+      <h1>Hello {props.nome} sua idade é {props.idade} isso é um título.</h1>
+      <h2>um subtítulo</h2>      
+      <MeuComboBox/>
+    </>
+  )
+  
 }
 
 export default App;
