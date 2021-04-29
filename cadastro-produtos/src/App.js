07f96e1e14118e8ef8da3a11b7a80bf4227e0162
William Silva <=>
2021-04-29 17:01:30 -0300

    21 e 22 - Adicionar navbar e uma home.

--- cadastro-produtos/src/App.js
+++ cadastro-produtos/src/App.js
@@ -1,79 +1,13 @@
 import React from 'react';
+import Navbar from './components/navbar';
+import Home from './views/home';
 
 function App() {
   return (
-    <table className="table table-hover">
-      <thead>
-        <tr>
-          <th scope="col">Type</th>
-          <th scope="col">Column heading</th>
-          <th scope="col">Column heading</th>
-          <th scope="col">Column heading</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr className="table-active">
-          <th scope="row">Active</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr>
-          <th scope="row">Default</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr className="table-primary">
-          <th scope="row">Primary</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr className="table-secondary">
-          <th scope="row">Secondary</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr className="table-success">
-          <th scope="row">Success</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr className="table-danger">
-          <th scope="row">Danger</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr className="table-warning">
-          <th scope="row">Warning</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr className="table-info">
-          <th scope="row">Info</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr className="table-light">
-          <th scope="row">Light</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-        <tr className="table-dark">
-          <th scope="row">Dark</th>
-          <td>Column content</td>
-          <td>Column content</td>
-          <td>Column content</td>
-        </tr>
-      </tbody>
-    </table>
+    <div className="container">
+      <Navbar />
+      <Home />
+    </div>
   );
 }
 
