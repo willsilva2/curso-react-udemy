2e84a6dfdc8e2df5b345756240f15114f3d56e96
William Silva <williamsilva2@outlook.com>
2021-04-30 19:05:33 -0300

    22 - Iniciando formulário de produtos

---
+++ cadastro-produtos/src/views/produtos/cadastro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class CadastroProduto extends React.Component {
+    render() {
+        <div class="card">
+            <div class="card-header">
+                Cadastro de Produto
+            </div>
+            <div class="card-body">
+            </div>
+        </div>
+    }
+}
+
+export default CadastroProduto;
