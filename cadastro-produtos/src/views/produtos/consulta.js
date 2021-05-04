b1d3c91f100b8c8de92d45c7721cb3e35cb9b4d2
William Silva <williamsilva2@outlook.com>
2021-05-03 22:26:14 -0300

    40. Transformar o card em um componente.

--- cadastro-produtos/src/views/produtos/consulta.js
+++ cadastro-produtos/src/views/produtos/consulta.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router';
 import ProdutoService from '../../app/produtoService';
+import Card from '../../components/card';
 
 class ConsultaProdutos extends React.Component {
 
@@ -16,7 +17,7 @@ class ConsultaProdutos extends React.Component {
     componentDidMount() {
         const produtos = this.service.obterProdutos();
         //equivale a produtos: produtos
-        this.setState({ produtos });
+        this.setState({ produtos });        
     }
 
     prepararEditar = (sku) => {        
@@ -32,42 +33,37 @@ class ConsultaProdutos extends React.Component {
 
     render() {
         return (
-            <div className="card">
-                <div className="card-header">
-                    Consulta Produtos
-                </div>
-                <div className="card-body">
-                    <table className="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Nome</th>
-                                <th>SKU</th>
-                                <th>Preço</th>
-                                <th>Fornecedor</th>
-                                <th>Ações</th>
-                            </tr>
-                        </thead>
-                        <tbody>  
-                            
-                            {this.state.produtos.map( (produto, index) => {                        
-                                return (                            
-                                    <tr key={index}>
-                                        <td>{produto.nome}</td>
-                                        <td>{produto.sku}</td>
-                                        <td>{produto.preco}</td>
-                                        <td>{produto.fornecedor}</td>
-                                        <td>
-                                            <button className="btn btn-primary" onClick={ () => this.prepararEditar(produto.sku) }>Editar</button>
-                                            <button className="btn btn-danger ml-1" onClick={ () => this.excluir(produto.sku) }>Remover</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}                  
-                            
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+            <Card header="Consulta Produtos">
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>SKU</th>
+                            <th>Preço</th>
+                            <th>Fornecedor</th>
+                            <th>Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>  
+                        
+                        { this.state.produtos.map( (produto, index) => {                        
+                            return (                            
+                                <tr key={index}>
+                                    <td>{produto.nome}</td>
+                                    <td>{produto.sku}</td>
+                                    <td>{produto.preco}</td>
+                                    <td>{produto.fornecedor}</td>
+                                    <td>
+                                        <button className="btn btn-primary" onClick={ () => this.prepararEditar(produto.sku) }>Editar</button>
+                                        <button className="btn btn-danger ml-1" onClick={ () => this.excluir(produto.sku) }>Remover</button>
+                                    </td>
+                                </tr>
+                            )
+                        })}                  
+                        
+                    </tbody>
+                </table>                
+            </Card>
         )
     }
 }
